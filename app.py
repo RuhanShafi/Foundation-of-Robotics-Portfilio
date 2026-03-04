c0c5e5b4d5746a49869d3da8cfcb0aedbfc370fc
Ruhan Shafi <imruhans@gmail.com>
2026-03-04 22:09:38 +1100

    Setup .tex scaffold

---
+++ app.py
@@ -0,0 +1,3 @@
+import rclpy  # ros library for python
+from rclpy.node import Node  # import the Node class from rclpy
+from std_msgs.msg import ColorRGBA  # import the ColorRGBA message type definition
